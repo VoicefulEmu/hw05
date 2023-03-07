cad158599e52fa40879b1db5155202f30d08dfd1
Ezra Lamoreaux <65139116+VoicefulEmu@users.noreply.github.com>
2023-03-07 12:44:54 -0700

    Solve problem 3

--- hw05.py
+++ hw05.py
@@ -72,6 +72,52 @@ def path_yielder(t, value):
             "*** YOUR CODE HERE ***"
 
 
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
+
+tree = lambda label, branches=[]: Tree(label, branches)
+label = lambda t: t.label
+branches = lambda t: t.branches
+print_tree = lambda t: print(t)
+
+
+
+
+
 def preorder(t):
     """Return a list of the entries in this tree in the order that they
     would be visited by a preorder traversal (see problem description).
@@ -83,6 +129,13 @@ def preorder(t):
     [2, 4, 6]
     """
     "*** YOUR CODE HERE ***"
+    if t is None:
+        return []
+    final_list = [label(t)]
+    for b in branches(t):
+        final_list.extend(preorder(b))
+    return final_list
+
 
 
 def generate_preorder(t):
@@ -131,49 +184,6 @@ def remainders_generator(m):
     11
     """
     "*** YOUR CODE HERE ***"
-
-
-class Tree:
-    """
-    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
-    >>> t.label
-    3
-    >>> t.branches[0].label
-    2
-    >>> t.branches[1].is_leaf()
-    True
-    """
-
-    def __init__(self, label, branches=[]):
-        for b in branches:
-            assert isinstance(b, Tree)
-        self.label = label
-        self.branches = list(branches)
-
-    def is_leaf(self):
-        return not self.branches
-
-    def __repr__(self):
-        if self.branches:
-            branch_str = ', ' + repr(self.branches)
-        else:
-            branch_str = ''
-        return 'Tree({0}{1})'.format(self.label, branch_str)
-
-    def __str__(self):
-        def print_tree(t, indent=0):
-            tree_str = '  ' * indent + str(t.label) + "\n"
-            for b in t.branches:
-                tree_str += print_tree(b, indent + 1)
-            return tree_str
-        return print_tree(self).rstrip()
-
-
-tree = lambda label, branches=[]: Tree(label, branches)
-label = lambda t: t.label
-branches = lambda t: t.branches
-print_tree = lambda t: print(t)
-
 
 def naturals():
     """A generator function that yields the infinite sequence of natural
